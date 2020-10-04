6af55749d1482849d91893b9c5f9234f37294776
Philip Jung <pjung16@Philips-MacBook-Pro.local>
2020-10-04 18:35:15 -0400

    deleted unneeded lines from store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../Components/counter/counterSlice';
 import entriesReducer from '../dataSlice';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     data: entriesReducer,
   },
 });
