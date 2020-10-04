dbe85203eb36a07b10edbbb8549f60bdc1b17492
Philip Jung <pjung16@Philips-MacBook-Pro.local>
2020-10-04 18:43:05 -0400

    Updates

---
+++ precache-manifest.b7d4acf32ba00f9ed07cac4e3bd36a0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8068dc8088d5a1cfd6b609feb9a52fdf",
+    "url": "/stackline-assessment/index.html"
+  },
+  {
+    "revision": "ef77fed9182a30c8cea7",
+    "url": "/stackline-assessment/static/css/main.ff380aa5.chunk.css"
+  },
+  {
+    "revision": "1c3e452c2a6382bad630",
+    "url": "/stackline-assessment/static/js/2.b5353d5c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/stackline-assessment/static/js/2.b5353d5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef77fed9182a30c8cea7",
+    "url": "/stackline-assessment/static/js/main.15e3d9ac.chunk.js"
+  },
+  {
+    "revision": "1c626d9f3f3b16bfe49e",
+    "url": "/stackline-assessment/static/js/runtime-main.63f53f46.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dfaaa449f712d3510d9ec484af4c375a.js"
+  "/stackline-assessment/precache-manifest.b7d4acf32ba00f9ed07cac4e3bd36a0e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/stackline-assessment/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
