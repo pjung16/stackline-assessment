620f36af14477e531d92722a75ed95e4f6ec579c
Philip Jung <pjung16@Philips-MacBook-Pro.local>
2020-10-04 18:38:20 -0400

    Updates

---
+++ precache-manifest.dfaaa449f712d3510d9ec484af4c375a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44ec9623c710019b838356078af3cc1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d212bf914a070661fa5",
+    "url": "/static/css/main.ff380aa5.chunk.css"
+  },
+  {
+    "revision": "1c3e452c2a6382bad630",
+    "url": "/static/js/2.b5353d5c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b5353d5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d212bf914a070661fa5",
+    "url": "/static/js/main.25e6abe5.chunk.js"
+  },
+  {
+    "revision": "c5e33494d1804b0aff6d",
+    "url": "/static/js/runtime-main.b325f3ba.js"
+  }
+]);
